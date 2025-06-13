5e4c6ae3062cbe24eed55d479d200d76b788b90a
Vigneshwaran Rajamanickam <vigneshwaranr23@centizen.com>
2025-06-13 17:50:59 +0530

    changes

--- protfolio/src/app/app.routes.ts
+++ protfolio/src/app/app.routes.ts
@@ -1,6 +1,12 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadComponent: () => import('./Components/landing/landing.component').then(m => m.LandingComponent) }
+  { path: '', redirectTo: 'home', pathMatch: 'full' },  // Redirect root to 'home'
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./Components/landing/landing.component').then(
+        (m) => m.LandingComponent
+      ),
+  },
 ];
